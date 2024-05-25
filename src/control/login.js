a92ffbbbb4e539c9aa68a6bc2e4ad859bcff6e1f
Michael Mena <pjrmenas@gmail.com>
2024-05-25 14:51:33 -0400

    fetch sesion completo

--- src/control/login.js
+++ src/control/login.js
@@ -3,52 +3,50 @@ const util = require('util');
 const query = util.promisify(conexion.query).bind(conexion);
 
 
-exports.verificar = (req, res)=>{
+exports.verificar = (req, res) => {
    try {
-    const {celd, pass} = req.body;
-    console.log(celd, pass)
-
-    query('SELECT * FROM admin WHERE cedula = ?', [celd], (err, result)=>{
-            if(result.length > 0){
-            if(pass == result[0].passw){
-                 res.render('index', {ok: true})
-            }else{
-                console.log(error);
-                res.render('index', {ok: false})
-            }
-            }else{
-                console.log(err);
-                res.render('index', {ok: false})
-            }
-    })
-
+       const { celd, pass } = req.body;
+       if (celd == '' || pass == '') {
+           res.send('vacio')
+       } else {
+           query('SELECT * FROM admin WHERE cedula = ?', [celd], (err, result) => {
+               if (result.length > 0) {
+                   if (pass == result[0].passw) {
+                       res.send('success')
+                   } else {
+                       res.send('err')
+                   }
+               } else {
+                   res.send('err')
+               }
+           })
+       }
    } catch (error) {
-    
-    console.log(error);
-
+       console.log(error);
+       res.status(500).send('Error en el servidor');
    }
 }
 
-exports.verificar = (req, res)=>{
-    try {
-     const {celd, pass} = req.body;
-     console.log(celd, pass);
-     if(celd == '' || pass == ''){
-        res.sen('vacio')
-     }
-     query('SELECT * FROM admin WHERE cedula = ?', [celd], (err, result)=>{
-             if(result.length > 0){
-             if(pass == result[0].passw){
-                res.send('OK'); 
-             }else{
-                res.send('err'); 
-             }
-             }else{
-                res.send('err'); 
-             }
-     })
-    } catch (error) {
-     console.log(error);
-     res.status(500).send('Error en el servidor'); // enviar una respuesta de error al frontend
-    }
- }
+// exports.verificar = (req, res)=>{
+//     try {
+//      const {celd, pass} = req.body;
+//      console.log(celd, pass);
+//      if(celd == '' || pass == ''){
+//         res.sen('vacio')
+//      }
+//      query('SELECT * FROM admin WHERE cedula = ?', [celd], (err, result)=>{
+//              if(result.length > 0){
+//              if(pass == result[0].passw){
+//                 res.send('OK'); 
+//              }else{
+//                 res.send('err'); 
+//              }
+//              }else{
+//                 res.send('err'); 
+//              }
+//      })
+//     } catch (error) {
+//      console.log(error);
+//      res.status(500).send('Error en el servidor'); // enviar una respuesta de error al frontend
+//     }
+//  }
